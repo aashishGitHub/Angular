7183327985c453284d9ac226c931c3ae0dea7c65
Aashish Kumar <aashish123pqr@gmail.com>
2018-06-30 16:38:30 +0530

    added css to mark parent child nesting in component
    .

--- first-app/src/app/parent-component/parent-component.component.ts
+++ first-app/src/app/parent-component/parent-component.component.ts
@@ -3,9 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parent-component',
   templateUrl: './parent-component.component.html',
-  styleUrls: ['./parent-component.component.css'],
-  styles:['bg-blue']
-  
+  styleUrls: ['./parent-component.component.css']    
 })
 export class ParentComponentComponent implements OnInit {
 
