887e7f9d92e3fc33c9b75d84896a5bb1931c5a82
Aashish Kumar <aashish123pqr@gmail.com>
2018-06-30 23:17:02 +0530

    first FormModule and bindings

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ParentComponentComponent } from './parent-component/parent-component.component';
@@ -16,7 +17,8 @@ import { ParentChildComponentComponent } from './parent-component/parent-child-c
     ParentChildComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
